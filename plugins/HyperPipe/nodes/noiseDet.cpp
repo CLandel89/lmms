a1374237b64c959f008d0ae03bb333516cf4390b
Chr L <titan1989@gmx.de>
2024-05-04 16:09:33 +0200

    wip-noise-det-2

--- plugins/HyperPipe/nodes/noiseDet.cpp
+++ plugins/HyperPipe/nodes/noiseDet.cpp
@@ -34,51 +34,89 @@ inline unique_ptr<HPNode> instantiateNoiseDet(HPModel* model, int model_i);
 
 struct HPNoiseDetModel : public HPModel::Node {
 	HPNoiseDetModel(Instrument* instrument) :
-			HPModel::Node(instrument)
+			HPModel::Node(instrument),
+			m_mingle(
+				57,
+				1, 0xffff, instrument, QString("mingle factor"))
 	{}
+	IntModel m_mingle;
 	unique_ptr<HPNode> instantiate(HPModel* model, int model_i) {
 		return instantiateNoiseDet(model, model_i);
 	}
 	string name() { return NOISE_DET_NAME; }
-	void load(int model_i, const QDomElement& elem) {}
-	void save(int model_i, QDomDocument& doc, QDomElement& elem) {}
+	void load(int model_i, const QDomElement& elem) {
+		QString is = "n" + QString::number(model_i);
+		m_mingle.loadSettings(elem, is + "_mingle");
+	}
+	void save(int model_i, QDomDocument& doc, QDomElement& elem) {
+		QString is = "n" + QString::number(model_i);
+		m_mingle.saveSettings(doc, elem, is + "_mingle");
+	}
 	bool usesPrev() { return false; }
 };
 
 class HPNoiseDet : public HPNode
 {
-	float m_iter = 0;
+	int32_t m_iter = 0;  // if ph and iter aren't separate, audible glitches will occur
+	float m_ph = 0;
 public:
 	HPNoiseDet(HPModel* model, int model_i, shared_ptr<HPNoiseDetModel> nmodel) :
-			HPNode()
+			HPNode(),
+			m_nmodel(nmodel)
 	{}
 	float processFrame(Params p) {
-		m_iter += p.freq / p.srate;
-		const float ph = hpposmodf(m_iter);
-		const uint8_t iter = m_iter < 0 ? -m_iter : m_iter;
-		// split the 8 bits of "iter" into 4 variables (A0,A1,B0,B1)
-		const uint8_t  iterA  = iter & 0xf;
-		uint8_t        iterA0 = iterA & 0x3;
-		const uint8_t  iterA1 = iterA >> 2;
-		const uint8_t  iterB  = (iter>>4) & 0xf;
-		uint8_t        iterB0 = iterB & 0x3;
-		const uint8_t  iterB1 = iterB >> 2;
-		// mingle some bits
-		iterA0 = ((iterA0 & 1) << 1) | (iterA0 >> 1);
-		iterB0 = ((iterB0 & 1) << 1) | (iterB0 >> 1);
-		// depending on the current quarter of the phase, output one of the 4
-		// (this is construed so that the first wave is a "square")
-		if (ph < 0.25f) {
-			return 1 - iterA0 / 4.0f;
+		uint16_t iter         = m_iter < 0 ? -m_iter : m_iter;
+		iter *= m_nmodel->m_mingle.value();
+		uint16_t gray = iter ^ (iter >> 1);  // https://de.wikipedia.org/wiki/Gray-Code#Generierung
+		// mingle, bitwise
+		if (iter & 1) {
+			iter = 0;
+			for (uint8_t b = 0; b < 8; b++) {
+				uint16_t bit = (gray >> b) & 1;
+				if (b & 1) {
+					bit = (~bit) & 1;
+				}
+				iter |= bit << b;
+			}
 		}
-		if (ph < 0.5f) {
-			return 1 - iterA1 / 4.0f;
+		else {
+			iter = 0;
+			for (uint8_t b = 0; b < 8; b++) {
+				uint16_t bit = (gray >> b) & 1;
+				if (!(b & 1)) {
+					bit = (~bit) & 1;
+				}
+				iter |= bit << b;
+			}
 		}
-		if (ph < 0.75f) {
-			return -1 + iterB0 / 4.0f;
+		// determine which bits to output
+		const float hph = hpposmodf(2*p.ph);
+		uint8_t step = 4 * hpposmodf(hph);
+		if (p.ph >= 0.5f) {
+			// the second half is backwards and mirrored
+			step = 3 - step;
 		}
-		return -1 + iterB1 / 4.0f;
+		uint8_t output = (iter >> (step*4)) & 0xf;
+		// https://de.wikipedia.org/wiki/Gray-Code#Gray-Code_zur%C3%BCckrechnen
+		uint8_t mask = output;
+		while (mask) {
+			mask >>= 1;
+			output ^= mask;
+		}
+		// inner state, output
+		float outputf;
+		if (p.ph >= 0.5f) {
+			outputf = -1 + 2.0f * output / 0xf;
+		}
+		else {
+			outputf = 1 - 2.0f * output / 0xf;
+		}
+		m_ph += p.freqMod / p.srate;
+		m_iter += floor(m_ph);
+		m_ph = hpposmodf(m_ph);
+		return outputf;
 	}
+	shared_ptr<HPNoiseDetModel> m_nmodel;
 };
 
 inline unique_ptr<HPNode> instantiateNoiseDet(HPModel* model, int model_i) {
@@ -91,8 +129,17 @@ inline unique_ptr<HPNode> instantiateNoiseDet(HPModel* model, int model_i) {
 
 class HPNoiseDetView : public HPNodeView {
 public:
-	HPNoiseDetView(HPView* view) {}
-	void setModel(weak_ptr<HPModel::Node> nmodel) {}
+	HPNoiseDetView(HPView* view) :
+			m_mingle(new LcdSpinBox(5, view, "mingle factor"))
+	{
+		m_widgets.emplace_back(m_mingle);
+	}
+	void setModel(weak_ptr<HPModel::Node> nmodel) {
+		auto modelCast = static_cast<HPNoiseDetModel*>(nmodel.lock().get());
+		m_mingle->setModel(&modelCast->m_mingle);
+	}
+private:
+	LcdSpinBox *m_mingle;
 };
 
 using Definition = HPDefinition<HPNoiseDetModel>;
